aa1572dc42a2d325773c1774e3510a0d26afef3d
elanakoren <3656487+elanakoren@users.noreply.github.com>
2026-08-02 13:08:36 -0700

    prompt for question and retrieve relevant chunks

--- src/rag/__init__.py
+++ src/rag/__init__.py
@@ -2,13 +2,18 @@ from datasets import load_dataset
 from rag.chunking import chunk_text
 from rag.transformer import encode
 from rag.store import store_chunks
+import chromadb
 
 def main() -> None:
+    question = input("Ask a question: ")
+    client = chromadb.PersistentClient(path="./chroma_db")
+    encoded_question = encode(question)
     ds = load_dataset("emozilla/pg19", split="train[:50]")
     chunked_text = []
     encoded_dataset = []
     for text in ds:
         chunked_text.extend(chunk_text(text))
+
     # TODO: Batch for performance
     for chunk in chunked_text:
         encoded_chunk = encode(chunk['text'])
@@ -19,4 +24,5 @@ def main() -> None:
             'position': chunk['position'],
             })
     store_chunks(encoded_dataset)
-    print(f"stored {len(encoded_dataset)} chunks")
+    collection = client.get_collection(name='pg19')
+    result = collection.query(query_embeddings=[encoded_question], n_results=5)
